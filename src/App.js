5f8a95467f7639021bdaab08b8dafccf3160b9d0
Ray Albright <ray.a.albright@gmail.com>
2018-10-13 18:29:33 -0500

    made timeline

--- src/App.js
+++ src/App.js
@@ -10,6 +10,28 @@ class App extends Component {
     return (
       <div className="container">
         <h1>Timeline</h1>
+        {/* Timeline */ }
+        <div className="d-flex justify-content-center">
+          <ul className="list-group list-group-flush w-50 ">
+            { this.state.chirps.map( chirp => (
+              <li className="list-group-item" key={ chirp.id }>
+                {/* Chirp */ }
+                <div className="media">
+                  <img src={ chirp.user.avatar } alt={ chirp.user.name } className="mr-3" />
+                  <div className="media-body">
+                    <h5 className="mt-0">
+                      <span>{ chirp.user.name }</span>
+                      <span>{ chirp.createdAt.toDateString() }</span>
+                    </h5>
+                    <p>{ chirp.body }</p>
+                  </div>
+                </div>
+                {/*Chirp*/ }
+              </li>
+            ) ) }
+          </ul>
+        </div>
+        {/* Timeline */ }
       </div>
     );
   }
